89685cc09128cd152118434e49282c49e5fac7a7
Toaster1123 <prokhorov.rostislav@gmail.com>
2024-12-12 11:38:56 +0300

    add loading pagination

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import NextTopLoader from 'nextjs-toploader';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}  flex items-center flex-col bg-gray-900`}>
         <div className="flex flex-col w-full min-h-screen max-w-[1250px] mx-auto  ">
+          <NextTopLoader color="#65a30d" showSpinner={false} />
           <Header />
           <main className="flex-grow flex bg-gray-200 flex-col ">{children}</main>
           <Footer />
