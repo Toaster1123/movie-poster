78aa2e7039cd3d166ce07e0dbb01568f445d5657
Toaster1123 <prokhorov.rostislav@gmail.com>
2025-04-02 12:37:26 +0300

    fix(build):fix for build

---
+++ app/_error/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ErrorPage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Ошибка</h1>
+      <p>Что-то пошло не так...</p>
+    </div>
+  );
+}
